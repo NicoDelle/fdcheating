51806cb161818456a94233dcda0c92c0c0654dfc
Iki <74684402+xshamsy@users.noreply.github.com>
2024-05-01 20:05:37 +0200

    Update esempio.py
    caricamento ip con la convenzione /netmask, struttura dati cambiata da dizionario a lista

--- esempio.py
+++ esempio.py
@@ -4,6 +4,14 @@ File di esempio per l'utilizzo dei moduli IP e routeTable
 import IP
 import routeTable
 
+table = ['192.168.1.0/10', '192.128.0.0/6', '0.0.0.0/0']
+ip = IP.IPaddress('192.168.1.10') # non è necessario fornire la netmask
+rt = routeTable.RouteTable(table)
+result = rt.match(ip)
+
+print("\n")
+for row in result:
+    print(row.ipraw)
 
 """ 
 SI può creare una tabella di routing come dizionario, fornendo coppie di oggetti IP e simbolici next hop (non serve implementari i next hop come veri IP).
@@ -11,7 +19,7 @@ SI può creare una tabella di routing come dizionario, fornendo coppie di oggett
 La sintassi per creare un oggetto IP è:
 ip = IP.IPaddress(IP, netmask=0) (si chiama la classe IPaddress del modulo IP, e gli si passa una stringa puntuata con un indirizzo ip decimale e ll'eventuale netmask, altrimenti settata ad una flag di -1). è disponibile anche un attributo per accedere al valore binario dell'IP, ip.ipBin.
 Per vedere entrambe le rappresentazioni si può direttamente chiamare print() su un oggetto IP
-"""
+
 table = {
     IP.IPaddress(
         '192.168.1.0', 
@@ -26,12 +34,13 @@ table = {
 
 ip = IP.IPaddress('192.168.1.10') # non è necessario fornire la netmask
 
-"""
 La tabella di routing è creata passando il dizionario di cui sopra. Altrimenti, si può invocare il costruttore (routeTable.RouteTable()) senza argomenti: in questo modo si aggiungeranno le info da CLI, al momento.
-"""
+
 rt = routeTable.RouteTable(table)
 
-"""
+
 Invocare il metodo .match(ip) su un oggetto RouteTable restituisce un dizionario contenente la corrispondenza tra l'indirizzo IP e il next hop simbolico.
-"""
+
 print(rt.match(ip))
+
+"""
